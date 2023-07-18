ba9177f8d38372b094a77f1fe9c3d59cc8b6733b
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-07-18 15:34:51 +0900

    feat: upgrade Apache Camel model to 4.0.0-RC1

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import dataformats from './dataformats-camel-model.json'
 import languages from './languages-camel-model.json'
 import components from './components-camel-model.json'
 
-const apacheCamelModelVersion = '3.21.0'
+const apacheCamelModelVersion = '4.0.0-RC1'
 
 export { definitions, rests, dataformats, languages, components, apacheCamelModelVersion }
