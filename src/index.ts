0c0c7ed803b69d79499f3d4f7aaa91076152d864
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-07-18 15:22:20 +0900

    feat: upgrade Apache Camel model to 3.21.0

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import components from './components-camel-model.json'
-import dataformats from './dataformats-camel-model.json'
 import definitions from './definitions-camel-model.json'
-import languages from './languages-camel-model.json'
 import rests from './rests-camel-model.json'
+import dataformats from './dataformats-camel-model.json'
+import languages from './languages-camel-model.json'
+import components from './components-camel-model.json'
 
-const apacheCamelModelVersion = '3.20.6'
+const apacheCamelModelVersion = '3.21.0'
 
-export { apacheCamelModelVersion, components, dataformats, definitions, languages, rests }
+export { definitions, rests, dataformats, languages, components, apacheCamelModelVersion }
