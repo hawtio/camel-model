0712fb4c1394cdb04a0d2f7a3f15a3a398a60278
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-10-20 16:45:50 +0900

    fix: access to schema elements are redundant
    Fix #3

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,37 +6,32 @@ describe('camel-model', () => {
   })
 
   test('components', () => {
-    const comps = components.components
-    expect(comps).toBeDefined()
-    expect(Object.keys(comps).length).toBeGreaterThan(100)
-    expect(Object.keys(comps)).toContain('direct')
+    expect(components).toBeDefined()
+    expect(Object.keys(components).length).toBeGreaterThan(100)
+    expect(Object.keys(components)).toContain('direct')
   })
 
   test('dataformats', () => {
-    const dfs = dataformats.dataformats
-    expect(dfs).toBeDefined()
-    expect(Object.keys(dfs).length).toBeGreaterThan(10)
-    expect(Object.keys(dfs)).toContain('json')
+    expect(dataformats).toBeDefined()
+    expect(Object.keys(dataformats).length).toBeGreaterThan(10)
+    expect(Object.keys(dataformats)).toContain('json')
   })
 
   test('definitions', () => {
-    const defs = definitions.definitions
-    expect(defs).toBeDefined()
-    expect(Object.keys(defs).length).toBeGreaterThan(30)
-    expect(Object.keys(defs)).toContain('choice')
+    expect(definitions).toBeDefined()
+    expect(Object.keys(definitions).length).toBeGreaterThan(30)
+    expect(Object.keys(definitions)).toContain('choice')
   })
 
   test('languages', () => {
-    const langs = languages.languages
-    expect(langs).toBeDefined()
-    expect(Object.keys(langs).length).toBeGreaterThan(10)
-    expect(Object.keys(langs)).toContain('simple')
+    expect(languages).toBeDefined()
+    expect(Object.keys(languages).length).toBeGreaterThan(10)
+    expect(Object.keys(languages)).toContain('simple')
   })
 
   test('rests', () => {
-    const rs = rests.rests
-    expect(rs).toBeDefined()
-    expect(Object.keys(rs).length).toBeGreaterThan(10)
-    expect(Object.keys(rs)).toContain('get')
+    expect(rests).toBeDefined()
+    expect(Object.keys(rests).length).toBeGreaterThan(10)
+    expect(Object.keys(rests)).toContain('get')
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import definitions from './definitions-camel-model.json'
-import rests from './rests-camel-model.json'
-import dataformats from './dataformats-camel-model.json'
-import languages from './languages-camel-model.json'
-import components from './components-camel-model.json'
+import { definitions } from './definitions-camel-model.json'
+import { rests } from './rests-camel-model.json'
+import { dataformats } from './dataformats-camel-model.json'
+import { languages } from './languages-camel-model.json'
+import { components } from './components-camel-model.json'
 
 const apacheCamelModelVersion = '4.0.1'
 
